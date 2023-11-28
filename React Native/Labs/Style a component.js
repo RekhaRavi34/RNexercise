c9d66566c0e3a7beef5c862588b59a1322857c92
RekhaRavi34 <rekharavi170397@gmail.com>
2023-11-28 15:55:06 +0530

    Create Style a component.js

---
+++ React Native/Labs/Style a component.js
@@ -0,0 +1,31 @@
+Exercise: Style a component
+Overview
+So far you have learned some of the core React Native components and have learned how to style them effectively. You’ve also discovered that inline styling, which is the practice of keeping styles within a component’s render, can become unmanageable with larger apps. Fortunately, you have the StyleSheet API at your disposal for keeping styles neatly in one place.   
+
+In this exercise, you will style a component and add the styles to the StyleSheet API.  
+
+Scenario   
+The Little Lemon app currently has some styling applied to the components. However, you have been asked to update the styling to achieve a different appearance. You should utilize the StyleSheet API to abstract the styles from the component’s render. Make sure that the component code is readable and clean.  
+
+Here is how the app currently appears:   
+
+Little Lemon app as displayed in emulator, with yellow and green styling.
+You will need to modify the app to match the styling shown below:
+
+Little Lemon app as displayed in the emulator with alternative styling.
+The colors displayed in the image above can be applied with the following values: 
+
+#EE9972, #333333, #EDEFEE, black and white.
+
+Starter Code
+You can download the starter code for this exercise from the zipped folder below:
+
+Instructions  
+Step 1: Update Styles of Components to match Scenario
+Your first step in this exercise is to update the styles of the given code to match the screenshot given in the scenario. Make sure to use the appropriate colors, padding and overall styling theme shown in the scenario.
+
+Step 2: Extract All Styles to StyleSheet API 
+In this step, you will extract all the styles per component into its own StyleSheet API. This will ensure that your component code is readable. A best practice is to provide meaningful names to all your styles so that you can easily identify the style or where it is used.
+
+Conclusion 
+By completing this exercise, you will demonstrate your understanding and ability to style React Native components. You will have demonstrated how to use the StyleSheet  API to abstract away styles while keeping the component code clean and readable.
