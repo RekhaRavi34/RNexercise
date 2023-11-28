5332200de9c826dfc3025558f1d49cf54b3ce653
RekhaRavi34 <rekharavi170397@gmail.com>
2023-11-28 22:53:11 +0530

    Create Render a large list using SectionList.js

---
+++ React Native/Labs/Render a large list using SectionList.js
@@ -0,0 +1,221 @@
+Exercise: Render a large list using SectionList
+Overview 
+So far, you have learned about the FlatList and SectionList components and how to render large lists using them. You’ve discovered that while FlatList is useful for rendering large lists performantly, SectionList adds the ability to separate list items into sections.
+
+In this exercise, you will expand on the example from an earlier exercise to display a large list of menu items with each item’s price. You will use the SectionList component within the Little Lemon app. 
+
+By doing this, each menu item will be categorized according to its item type and will display section headers such as Appetizers, Main dishes and so on.
+
+Scenario 
+The Little Lemon app needs to display a large list of menu items on the screen, along with the price of each item, sorted into sections. You have been asked to use the SectionList component to render these menu items efficiently. Make sure that the component you write is readable and clean.
+
+Below is the array of menu items, along with the price for each item.
+
+  const menuItemsToDisplay = [
+  {
+    title: 'Appetizers',
+    data: [
+      { name: 'Hummus', price: '$5.00' },
+      { name: 'Moutabal', price: '$5.00' },
+      { name: 'Falafel', price: '$7.50' },
+      { name: 'Marinated Olives', price: '$5.00' },
+      { name: 'Kofta', price: '$5.00' },
+      { name: 'Eggplant Salad', price: '$8.50' },
+    ],
+  },
+  {
+    title: 'Main Dishes',
+    data: [
+      { name: 'Lentil Burger', price: '$10.00' },
+      { name: 'Smoked Salmon', price: '$14.00' },
+      { name: 'Kofta Burger', price: '$11.00' },
+      { name: 'Turkish Kebab', price: '$15.50' },
+    ],
+  },
+  {
+    title: 'Sides',
+    data: [
+      { name: 'Fries', price: '$3.00', id: '11K' },
+      { name: 'Buttered Rice', price: '$3.00' },
+      { name: 'Bread Sticks', price: '$3.00' },
+      { name: 'Pita Pocket', price: '$3.00' },
+      { name: 'Lentil Soup', price: '$3.75' },
+      { name: 'Greek Salad', price: '$6.00' },
+      { name: 'Rice Pilaf', price: '$4.00' },
+    ],
+  },
+  {
+    title: 'Desserts',
+    data: [
+      { name: 'Baklava', price: '$3.00' },
+      { name: 'Tartufo', price: '$3.00' },
+      { name: 'Tiramisu', price: '$5.00' },
+      { name: 'Panna Cotta', price: '$5.00' },
+    ],
+  },
+];
+
+Use this array to render the items within the screen. 
+
+The colors displayed in the images above can be applied using the following values:
+
+#F4CE14, #EE9972, #333333, #EDEFEE, black and white.
+
+Starter Code: 
+You can download the starter code for this exercise, from the zipped folder below:
+
+Instructions  
+Step 1: Update MenuItems component to display new list of menu items and use SectionList
+To complete this exercise, you’ll first need to update the MenuItems component to display the menu items and price. 
+
+Within this component, use the array provided in this scenario to pass to the data prop of the SectionList component. Then configure the renderItem prop of the SectionList component to render each item’s name as well as the price per item. 
+
+Hint: You can create multiple components within the same file to keep code clean.
+
+Step 2: Render Section Header
+In this step, you will use the SectionList component to render the section header. The headers are provided within the title property in the array menuItemsToDisplay. Utilize it to render the headers for each section of menu items. Make sure that each menu item is now displayed inside the appropriate section.
+
+Step 3: Style the component
+In this step, you will style the new component that you have created to match the screenshots. Make sure to provide meaningful names to all your styles.
+
+Conclusion 
+By completing this exercise, you will demonstrate your understanding and ability to configure and utilize the SectionList component to render a large list of items by section. 
+
+Solution: Render a large list using SectionList
+
+MenuItems.js
+
+import React from 'react';
+
+import { View, Text, StyleSheet, SectionList } from 'react-native';
+
+const menuItemsToDisplay = [
+  {
+    title: 'Appetizers',
+    data: [
+      { name: 'Hummus', price: '$5.00' },
+      { name: 'Moutabal', price: '$5.00' },
+      { name: 'Falafel', price: '$7.50' },
+      { name: 'Marinated Olives', price: '$5.00' },
+      { name: 'Kofta', price: '$5.00' },
+      { name: 'Eggplant Salad', price: '$8.50' },
+    ],
+  },
+  {
+    title: 'Main Dishes',
+    data: [
+      { name: 'Lentil Burger', price: '$10.00' },
+      { name: 'Smoked Salmon', price: '$14.00' },
+      { name: 'Kofta Burger', price: '$11.00' },
+      { name: 'Turkish Kebab', price: '$15.50' },
+    ],
+  },
+  {
+    title: 'Sides',
+    data: [
+      { name: 'Fries', price: '$3.00', id: '11K' },
+      { name: 'Buttered Rice', price: '$3.00' },
+      { name: 'Bread Sticks', price: '$3.00' },
+      { name: 'Pita Pocket', price: '$3.00' },
+      { name: 'Lentil Soup', price: '$3.75' },
+      { name: 'Greek Salad', price: '$6.00' },
+      { name: 'Rice Pilaf', price: '$4.00' },
+    ],
+  },
+  {
+    title: 'Desserts',
+    data: [
+      { name: 'Baklava', price: '$3.00' },
+      { name: 'Tartufo', price: '$3.00' },
+      { name: 'Tiramisu', price: '$5.00' },
+      { name: 'Panna Cotta', price: '$5.00' },
+    ],
+  },
+];
+
+const Item = ({ name, price }) => (
+  <View style={menuStyles.innerContainer}>
+    <Text style={menuStyles.itemText}>{name}</Text>
+    <Text style={menuStyles.itemText}>{price}</Text>
+  </View>
+);
+
+const MenuItems = () => {
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
+
+  const renderSectionHeader = ({ section: { title } }) => (
+    <View style={menuStyles.headerStyle}>
+      <Text style={menuStyles.sectionHeader}>{title}</Text>
+    </View>
+  );
+
+  return (
+    <View style={menuStyles.container}>
+      <SectionList
+        sections={menuItemsToDisplay}
+        keyExtractor={(item, index) => item + index}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}></SectionList>
+    </View>
+  );
+};
+
+const menuStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  itemText: {
+    color: '#F4CE14',
+    fontSize: 20,
+  },
+  headerStyle: {
+    backgroundColor: '#F4CE14',
+  },
+  sectionHeader: {
+    color: 'black',
+    fontSize: 26,
+    flexWrap: 'wrap',
+    textAlign: 'center',
+  },
+});
+
+export default MenuItems;
+
+
+App.js:
+
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import MenuItems from './components/MenuItems';
+
+export default function App() {
+  return (
+    <>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <MenuItems />
+      </View>
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
+  
